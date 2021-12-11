f0caddda0795768c6989eb36d254afe3aba6ecae
delanovictor <delanovictor3@gmail.com>
2021-12-10 21:25:36 -0300

    fixed logic

--- src/cards.js
+++ src/cards.js
@@ -7,8 +7,8 @@ const deck = [
 
 const startHandSize = 5;
 
-let draws = 0
 
+//Select 5 random cards, gives a score and returns a array with the cards names
 exports.draw = function(){
     let cardsInHand = []
 
@@ -25,64 +25,139 @@ exports.draw = function(){
 
     const result = evaluate(cardsInHand)
 
-
-    draws++
-
-
     return {
         cards: getCards(cardsInHand),
         code: result.code,
-        text: result.text,
-        tryNumber: draws
+        text: result.text
     }
 }
 
+//Gives the cards a score (pair, flush, fullhouse etc)
 function evaluate (cardsInHand){
     let result = {
         code: 0,
         text: ''
     }
 
-    if(areInTheSameSuite(cardsInHand)){
-        if(areInOrder(cardsInHand)){
-            if(hasCard(cardsInHand, 'A', '*')){
-                result = {
-                    code: 9,
-                    text: 'Royal Flush'
-                }
-            }else{
-                result = {
-                    code: 8,
-                    text: 'Straigth Flush'
-                }
+    const sameSuit = areInTheSameSuit(cardsInHand)
+    const inOrder = areInOrder(cardsInHand)
+
+    if(sameSuit && inOrder){
+        if(hasCard(cardsInHand, 'A', '*')){
+            result = {
+                code: 9,
+                text: 'Royal Flush'
             }
         }else{
             result = {
-                code: 7,
-                text: 'Flush'
+                code: 8,
+                text: 'Straigth Flush'
             }
         }
+    }else if (inOrder){
+        result = {
+            code: 7,
+            text: 'Straigth'
+        }
+    } else if(sameSuit){
+        result = {
+            code: 7,
+            text: 'Flush'
+        }
     }else{ 
-        result = getMatches(cardsInHand)
+        result = searchMatches(cardsInHand)
     }
 
     return result;
 }
 
-function areInTheSameSuite  (cardsInHand){
+//Search for multiple cards of the same value (pair, triple of a kind, four of a kind, fullhouse)
+function searchMatches(cardsInHand){
+    let matches = []
+
+    cardsInHand.forEach((card, index) => {
+        cardsInHand.forEach((otherCard, index2) =>{
+            if(index != index2){
+                if(getCardNumber(card) == getCardNumber(otherCard)){
+                    if(matches.indexOf(card + '&' + otherCard) == -1 && matches.indexOf(otherCard + '&' + card) == -1){
+                        matches.push(card + '&' + otherCard)
+                    }
+                }
+            }
+        })
+    });
+    let text = 'Nothing'
+
+    switch(matches.length){
+        case 1:
+            text = 'One Pair (' + getCardNumber(matches[0].split('&')[0]) + ')'
+        break;
+        case 2:
+            text = 'Two Pair (' + getCardNumber(matches[0].split('&')[0]) + ') & (' + getCardNumber(matches[1].split('&')[0])  + ')'
+        break;
+        case 3:
+            text = 'Three of a Kind (' + getCardNumber(matches[0].split('&')[0]) + ')'
+        break;
+        case 4:
+            let cardNumberA = getCardNumber(matches[0].split('&')[0])
+
+            let cardNumberB = ''
+
+            let threeOfAKind = ''
+            let pair = ''
+
+            let counter = 0
+
+            matches.forEach(item=>{
+                if(getCardNumber(item.split('&')[0]) != cardNumberA){
+                    cardNumberB = getCardNumber(item.split('&')[0])
+                    counter++;
+                }else{
+                    counter--;
+                }
+            })
+
+            if(counter > 0){
+                threeOfAKind = cardNumberB
+                pair = cardNumberA
+            }else{
+                threeOfAKind = cardNumberA
+                pair = cardNumberB
+            }
+
+            text = 'Full House (' + threeOfAKind + ') & (' + pair + ')'
+        break;
+        case 6:
+            text = 'Four of a Kind (' + getCardNumber(matches[0].split('&')[0]) + ')'
+        break;
+        default:
+            text = 'High Card (' +  getHighCard(cardsInHand) + ')'
+
+    }
+
+    return {
+        code: matches.length,
+        text: text
+    }
+    
+}
+
+//Returns true if all cards are in the same suit
+function areInTheSameSuit  (cardsInHand){
     let suit = null;
 
     for(let card of cardsInHand){
         if(suit  == null)
-            suit = defineSuite(card);
+            suit = getSuit(card);
 
-        if(suit != defineSuite(card))
+        if(suit != getSuit(card))
             return false
     }
 
     return true
 }
 
+//Returns true if have their value in order
 function areInOrder (cardsInHand) {
     let highest = 0;
     let inOrder = true;
@@ -130,8 +205,9 @@ function areInOrder (cardsInHand) {
 
     return inOrder
 }
- 
-function defineSuite (cardIndex) {
+
+//Get suit of card
+function getSuit (cardIndex) {
     let suit = null
 
     if(cardIndex < 13){
@@ -152,10 +228,12 @@ function defineSuite (cardIndex) {
     return suit
 }
 
-function defineCardNumber (cardIndex) {
+//Get mumber (on the deck array) of the card
+function getCardNumber (cardIndex) {
     return  deck[cardIndex].split('_')[0]
 }
 
+//Get an array of the name of the cards
 function getCards  (cardsInHand) {
     let tempArray = []
 
@@ -166,6 +244,40 @@ function getCards  (cardsInHand) {
     return tempArray
 }
 
+//Get the highest card in the hand 
+function getHighCard(cardsInHand){
+    let highestCardIndex;
+    let highestCardValue = 0
+
+    cardsInHand.forEach((card, index)=>{
+        let cardValue = getCardNumber(card)
+    
+        switch(cardValue){
+            case 'A':
+                cardValue = 14;
+            break;
+            case 'K':
+                cardValue = 13;
+            break;
+            case 'Q':
+                cardValue = 12;
+            break;
+            case 'J':
+                cardValue = 11;
+            break;
+        }
+
+        if(cardValue > highestCardValue){
+            highestCardValue = cardValue 
+            highestCardIndex =  index
+        }
+    })
+
+    return  getCardNumber(cardsInHand[highestCardIndex])
+}
+
+//Returns true if it has a card in the 'cards' array that follow the rules of 'value' and 'suit'
+// * stands for any
 function hasCard (cards, value, suit){
     let subject = ''
 
@@ -194,70 +306,3 @@ function hasCard (cards, value, suit){
     return false
 }
 
-function getMatches(cardsInHand){
-    let matches = []
-
-    cardsInHand.forEach((card, index) => {
-        cardsInHand.forEach((otherCard, index2) =>{
-            if(index != index2){
-                if(defineCardNumber(card) == defineCardNumber(otherCard)){
-                    if(matches.indexOf(card + '&' + otherCard) == -1 && matches.indexOf(otherCard + '&' + card) == -1){
-                        matches.push(card + '&' + otherCard)
-                    }
-                }
-            }
-        })
-    });
-    let text = 'Nothing'
-
-    switch(matches.length){
-        case 1:
-            text = 'One Pair (' + defineCardNumber(matches[0].split('&')[0]) + ')'
-        break;
-        case 2:
-            text = 'Two Pair (' + defineCardNumber(matches[0].split('&')[0]) + ') & (' + defineCardNumber(matches[1].split('&')[0])  + ')'
-        break;
-        case 3:
-            text = 'Three of a Kind (' + defineCardNumber(matches[0].split('&')[0]) + ')'
-        break;
-        case 4:
-            let cardNumberA = defineCardNumber(matches[0].split('&')[0])
-
-            let cardNumberB = ''
-
-            let threeOfAKind = ''
-            let pair = ''
-
-            let counter = 0
-
-            matches.forEach(item=>{
-                if(defineCardNumber(item.split('&')[0]) != cardNumberA){
-                    cardNumberB = defineCardNumber(item.split('&')[0])
-                    counter++;
-                }else{
-                    counter--;
-                }
-            })
-
-            if(counter > 0){
-                threeOfAKind = cardNumberB
-                pair = cardNumberA
-            }else{
-                threeOfAKind = cardNumberA
-                pair = cardNumberB
-            }
-
-            text = 'Full House (' + threeOfAKind + ') & (' + pair + ')'
-        break;
-        case 6:
-            text = 'Four of a Kind (' + defineCardNumber(matches[0].split('&')[0]) + ')'
-        break;
-
-    }
-
-    return {
-        code: matches.length,
-        text: text
-    }
-    
-}
